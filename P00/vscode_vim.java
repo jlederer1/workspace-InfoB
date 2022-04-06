e819c3629d60b2c00351f49648c54c43c4e4d270
jlederer1 <jlederer@uni-osnabrueck.de>
2022-04-06 15:17:29 +0200

    [Added vscode vim notes]

---
+++ P00/vscode_vim.java
@@ -0,0 +1,30 @@
+// Neccessary vim commands 
+
+/*
+VIM COMMANDS
+
+hjkl                - navigation (left, down, up, right)
+i|:|v| Esc (Caps)   - mode (insert, command, visual, normal)
+    Shift+i             - insert from start of line 
+    Shift+a             - insert from end of line
+    i|a                 - insert left/right from cursor  
+// Neccessary vim commands 
+
+/*
+VIM COMMANDS
+
+hjkl                - navigation (left, down, up, right)
+i|:|v| Esc (Caps)   - mode (insert, command, visual, normal)
+    Shift+i             - insert from start of line 
+    Shift+a             - insert from end of line
+    i|a                 - insert left/right from cursor  
+
+x|dd|r|u            - delete (del char/line), replace ("2|2" r5 -> "25"), undo
++p                  - paste from system clipboard
+
+:set number         - enumerate lines
+    :5                  - goto line 5
+
+:w|:!       - save, run (e.g. :!javac hello.java ?)
+
+*/
