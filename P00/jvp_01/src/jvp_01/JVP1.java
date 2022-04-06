9bdd54121fb90468cc7f63f9f2687a625043ec34
jlederer1 <jlederer@uni-osnabrueck.de>
2022-04-06 18:35:19 +0200

    [-]

---
+++ P00/jvp_01/src/jvp_01/JVP1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jvp_01;
+
+/**
+ * @author Jakob
+ *
+ */
+public class JVP1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
---
+++ P00/jvp_01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jakob
+ *
+ */
+module jvp_01 {
+}
